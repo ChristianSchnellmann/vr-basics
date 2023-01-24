1e7796e07fa0506c28944551129af5a6feab523c
LAPTOP-HC9NTHIR\medo <christian.schnellmann@hslu.ch>
2023-01-24 17:02:24 +0100

    added steering wheel

--- Assets/Scripts/06 Boarding/BoardMovement.cs
+++ Assets/Scripts/06 Boarding/BoardMovement.cs
@@ -8,11 +8,16 @@ public class BoardMovement : MonoBehaviour
     public float speed = 2;
     public Transform boardTransform;
     public float rotationStrength = 0.01f;
+    Quaternion defaultWheelRotation;
+    public Transform steeringWheelTransform;
+    public float steeringWheelSensitivity = 0.01f;
 
 
     void Start()
     {
         characterController = GetComponent<CharacterController>();
+        if(steeringWheelTransform != null)
+            defaultWheelRotation = steeringWheelTransform.localRotation;
     }
 
     // Update is called once per frame
@@ -24,6 +29,9 @@ public class BoardMovement : MonoBehaviour
         {
             rotationAngle = rotationAngle - 360;
         }
+        if (steeringWheelTransform != null)
+            steeringWheelTransform.localRotation = Quaternion.EulerAngles(0, 0, rotationAngle * steeringWheelSensitivity) * defaultWheelRotation;
+
         transform.RotateAround(Vector3.up, -rotationAngle * Time.deltaTime * rotationStrength);
         characterController.Move(transform.forward * speed * Time.deltaTime);
         
